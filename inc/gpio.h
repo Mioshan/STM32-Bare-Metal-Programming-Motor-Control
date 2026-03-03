841559b6a072f343b6d45e4f8e4a17c287fc4e33
Kristofer-Lee-Pascua <krispascua21@gmail.com>
2026-03-03 00:39:51 -0800

    Set To-Do instructions for next time.

--- inc/gpio.h
+++ inc/gpio.h
@@ -3,4 +3,8 @@
 
 #include "stm32f4xx.h"
 
+void GPIO_Init(void)
+{
+    /*Initiate a PC13 pin for simple set/clear check*/
+}
 #endif 
--- inc/pwm.h
+++ inc/pwm.h
@@ -3,4 +3,12 @@
 
 #include "stm32f4xx.h"
 
+/*Register Bit sets*/
+
+void PWM_Init(void)
+{
+    /*Configure AF GPIO Pins and TIMx_CHy in PWM Mode 1*/
+
+    /*Develop this next day*/
+}
 #endif
